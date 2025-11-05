d6d6d67b8c66703767783277a4b0b7816929143c
mishraindresh963-star <mishraindresh963@gmail.com>
2025-11-05 18:39:11 +0530

    Delete Frontend directory

--- Frontend/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Navbar() {
-  return (
-    <nav className="bg-blue-600 text-white p-3 text-center shadow-lg">
-      <h1 className="text-xl font-bold">🏥 Smart Hospital</h1>
-    </nav>
-  );
-}
